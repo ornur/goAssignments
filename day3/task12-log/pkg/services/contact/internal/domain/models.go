136acb13f76f9e0629f9dd32d1f648f33256e682
Nurda <83532144+ornur@users.noreply.github.com>
2024-02-17 18:14:07 +0600

    Added logging to handlers

---
+++ day3/task12-log/pkg/services/contact/internal/domain/models.go
@@ -0,0 +1,14 @@
+package domain
+
+type Contact struct {
+    ID        string
+    FullName  string
+    FirstName string
+    Patronymic string
+    PhoneNumber string
+}
+
+type Group struct {
+    ID   string
+    Name string
+}
